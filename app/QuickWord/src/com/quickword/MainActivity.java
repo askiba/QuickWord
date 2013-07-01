e6b7f37368dcfb98f13e8b2a07e812ae751beef4
Anna Skiba <annam.skiba@gmail.com>
2013-07-01 18:45:28 +0200

    Adding, translating, storing and viewing words

--- app/QuickWord/src/com/quickword/MainActivity.java
+++ app/QuickWord/src/com/quickword/MainActivity.java
@@ -1,12 +1,13 @@
 package com.quickword;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends Activity {
-
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +23,13 @@ public class MainActivity extends Activity {
     }
     
     public void addWord(View view){
-    	
+    	Intent addWordIntent = new Intent(MainActivity.this,AddWord.class);
+    	startActivity(addWordIntent);
     }
     
     public void learn(View view){
-    	
+    	Intent learnIntent = new Intent(MainActivity.this,Learn.class);
+    	startActivity(learnIntent);
     }
 
     public void download(View view){
